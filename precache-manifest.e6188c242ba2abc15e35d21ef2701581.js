fa792c81f4d1900c055630a8b6993d6a206662fa
DevVatu <devvatu@gmail.com>
2020-08-14 16:36:03 +0200

    Updates

---
+++ precache-manifest.e6188c242ba2abc15e35d21ef2701581.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd283ec4da30365d4b4dc92ca05e8ff4",
+    "url": "/Phone-Shop-Prototype/index.html"
+  },
+  {
+    "revision": "b966fd9579a09e296801",
+    "url": "/Phone-Shop-Prototype/static/css/main.0769f0a9.chunk.css"
+  },
+  {
+    "revision": "69f24c3946259d750a17",
+    "url": "/Phone-Shop-Prototype/static/js/2.84836ca5.chunk.js"
+  },
+  {
+    "revision": "f01cf20f87878beb20bf2f939f8352f1",
+    "url": "/Phone-Shop-Prototype/static/js/2.84836ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b966fd9579a09e296801",
+    "url": "/Phone-Shop-Prototype/static/js/main.ec93c79a.chunk.js"
+  },
+  {
+    "revision": "355231fd8fa82e272806",
+    "url": "/Phone-Shop-Prototype/static/js/runtime-main.551ec933.js"
+  },
+  {
+    "revision": "34a83a5747a79a88423d7cede8052404",
+    "url": "/Phone-Shop-Prototype/static/media/honor20.34a83a57.png"
+  },
+  {
+    "revision": "0005ae3f7de50e7ae33a72e03005a1ed",
+    "url": "/Phone-Shop-Prototype/static/media/honor30.0005ae3f.png"
+  },
+  {
+    "revision": "9d1a0b7fece5f30f9ab027bae8f4d78c",
+    "url": "/Phone-Shop-Prototype/static/media/huaweiP20.9d1a0b7f.png"
+  },
+  {
+    "revision": "467ea910e3bb2be59096d84cfb93cc2f",
+    "url": "/Phone-Shop-Prototype/static/media/huaweiP30.467ea910.png"
+  },
+  {
+    "revision": "dc8bd4cf69ab71ae5afb78fc961247a1",
+    "url": "/Phone-Shop-Prototype/static/media/iphone11proMax.dc8bd4cf.png"
+  },
+  {
+    "revision": "1d3de595fb35e08aff8b77b0cd7e2092",
+    "url": "/Phone-Shop-Prototype/static/media/iphone7plus.1d3de595.png"
+  },
+  {
+    "revision": "05da3d5f8b5ce72c0c744ffb59c73ef2",
+    "url": "/Phone-Shop-Prototype/static/media/iphoneSe2020.05da3d5f.png"
+  },
+  {
+    "revision": "c44ea6faf760b6cad4c35aa01722edbb",
+    "url": "/Phone-Shop-Prototype/static/media/iphoneX.c44ea6fa.png"
+  },
+  {
+    "revision": "4d2ffcdc7547bcc195993d6686bc9318",
+    "url": "/Phone-Shop-Prototype/static/media/samsungGalaxyNote10.4d2ffcdc.png"
+  },
+  {
+    "revision": "151a6783f0a12d4a33c44e6d6048bec4",
+    "url": "/Phone-Shop-Prototype/static/media/samsungGalaxyS20.151a6783.jpg"
+  },
+  {
+    "revision": "8f97072139b07cbab8a0f6e5a1593113",
+    "url": "/Phone-Shop-Prototype/static/media/samsungGalaxyS20Ultra.8f970721.png"
+  }
+]);
